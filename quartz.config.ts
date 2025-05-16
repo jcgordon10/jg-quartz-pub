e26f7f016e1bae743e4af9278265d8c6109afd81
Jackson Gordon <jcgordon10@gmail.com>
2025-05-15 20:27:25 -0400

    Quartz sync: May 15, 2025, 8:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jg-quartz-pub",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
